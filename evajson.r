79389d993f5ae6dea758e09c811c9344da6c1cf8
jsingh0202 <jaskirat@smaconsulting.ca>
2024-10-22 13:10:51 -0600

    updated script to provide pcl invoice data from eva

--- evajson.r
+++ evajson.r
@@ -1,26 +1,58 @@
 library(tidyverse)
 library(jsonlite)
 
-jsonData = fromJSON("data/ProjectAnalysis.json")
-df = as.data.frame(jsonData)
+json_data <- fromJSON("data/ProjectAnalysis.json")
+df <- as.data.frame(json_data)
 
 df <- filter(df, isLeaf == "True")
 df <- filter(df, grepl("04-05-07-01-03", code))
 
+# convert the data to a longer format to import to google sheets
+eva_pcl_longer <- df %>%
+  select(name, code, plannedCost, periods, actualCost,
+         actualCostPeriod, actualProgress, plannedProgress) %>%
+  separate_longer_delim(c(periods, actualCost, actualCostPeriod,
+                          actualProgress, plannedProgress), delim = ",") %>%
+  mutate(across(c(periods, actualCost, actualCostPeriod,
+                  actualProgress, plannedProgress),
+                ~ as.numeric(as.character(.)))) %>%
+  filter(actualCostPeriod > 0) %>%
+  arrange(periods)
+
+write.csv(eva_pcl_longer, "eva_pcl_longer.csv", row.names = FALSE)
+
+# eva_pcl <- df %>% select(name, code, plannedCost, periods, actualCost,
+#                          actualCostPeriod, actualProgress, plannedProgress)
+# 
+# eva_pcl_longer <- eva_pcl %>% 
+#                   separate_longer_delim(c(periods, actualCost,
+#                                           actualCostPeriod, actualProgress,
+#                                           plannedProgress), delim=",")
+# 
+# eva_pcl_longer$periods <- as.numeric(as.character(eva_pcl_longer$periods))
+# eva_pcl_longer$actualCostPeriod <- as.numeric(as.character(eva_pcl_longer$actualCostPeriod))
+# eva_pcl_longer$actualProgress <- as.numeric(as.character(eva_pcl_longer$actualProgress))
+# eva_pcl_longer$plannedProgress <- as.numeric(as.character(eva_pcl_longer$plannedProgress))
+# eva_pcl_longer$actualCost <- as.numeric(as.character(eva_pcl_longer$actualCost))
+# 
+# eva_pcl_longer <- eva_pcl_longer[order(eva_pcl_longer$periods), ]
+# 
+# eva_pcl_longer <- eva_pcl_longer %>% filter(actualCostPeriod > 0)
+
+# graph the planned costs of the summary tas# graph the planned costs of the summary tas# graph the planned costs of the summary tasks
+orderList <- aggregate(list(plannedCost=df$plannedCost), 
+                       by=list(parentName=df$parentName), 
+                       FUN=sum)
+
+p <- orderList %>% 
+     ggplot(aes(x = reorder(parentName, plannedCost),
+                 y = plannedCost)) + 
+     geom_col() + scale_y_continuous(labels = scales::label_dollar()) +
+     coord_flip() +
+     labs(title = "Planned Cost of Task Summaries under PCL Main Contract",
+          x = "Task Summaries",
+          y = "Planned Cost")
+p
+
 # Define the range of column names
 nested_col_names <- colnames(df)[14:ncol(df)]
-
-# Function to clean a single column
-clean_column <- function(col) {
-  data_list <- as.numeric(unlist(strsplit(col, ",")))
-  # Return only non-zero values as a list
-  return(list(data_list[data_list != 0]))
-}
-
-# Apply the function to the specified columns using mutate and across
-dfx <- df %>%
-  mutate(across(all_of(nested_col_names), ~ clean_column(.)))
-
-# Apply the function to the specified columns using mutate and across
-dfx <- dfx %>%
-  mutate(across(all_of(nested_col_names), ~ unnest_longer(.)))
